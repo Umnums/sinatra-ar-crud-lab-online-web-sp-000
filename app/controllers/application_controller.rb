cec63d3b704d0a4af25c8b7fc947ca4f10771761
Joseph Kao <josephtkao@gmail.com>
2020-08-12 18:43:05 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,11 +9,11 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+    redirect to "/articles"
   end
 
   get "/articles" do
     @articles=Article.all
-
     erb :index
   end
 
@@ -27,12 +27,25 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/articles" do
-    @article=Article.new(params)
-    erb :show
+    @article=Article.create(params)
+    redirect to "/articles/#{@article.id}"
   end
 
   get '/articles/:id/edit' do
+    @article= Article.find(params[:id])
     erb :edit
+  end
+
+  # update
+  patch "/articles/:id" do
+    @article = Article.find(params[:id])
+    @article.update(params[:article])
+    redirect to "/articles/#{@article.id}"
+  end
+
+  delete '/articles/:id' do
+    @article = Article.find(params[:id]).delete
+    redirect to "/articles"
   end
 
 end
